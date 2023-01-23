7331fab8779f1b5bea818ed1440b48f283859af6
copa <carpeas@outlook.es>
2023-01-23 19:07:59 +0100

    Ups... este commit está mal.

--- hola.php
+++ hola.php
@@ -2,6 +2,7 @@
 //echo "Hola Mundo\n";
 //@print "Hola {$argv[1]}\n";
 // El nombre por defecto es Mundo
+// Este comentario está mal y hay que borrarlo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
 ?>
